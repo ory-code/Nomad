4c3d07521b2dd4e01eecdc4491039a4dfa410727
ory-code <70527030+ory-code@users.noreply.github.com>
2022-09-27 12:15:44 +0200

    init Nav Bar with animated menu bar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+     <NavBar/>
+     <div className="menu-icon">
+
+     </div>
+    </div>
+  );
+
+}
+
+export default App;
